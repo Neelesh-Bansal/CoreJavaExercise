1f705407018632dc15dce1e5dc4d355214fcaae6
Neelesh-Bansal <neelesh.bansal@tothenew.com>
2018-02-14 19:27:47 +0530

    Changes done

--- Exer5.java
+++ Exer5.java
@@ -1,7 +1,16 @@
 public class Exer5 {
     public static void main(String args[]) {
-    String[] array1={"hello","world","new","memories"};
-    String[] array2={"welcome","to","new","TTN","world"};
+    String[] array1={"hello","world","world","new","memories"};
+    String[] array2={"welcome","to","new","TTN","world","world"};
+    for(int i=0;i<array1.length-1;i++){
+        for(int j=i+1;j<array1.length;j++){
+            if( (array1[i].equals(array1[j])) && (i != j) )
+            {
+                array1[i]="null";
+                break;
+            }
+        }
+    }
     for(int i=0;i<array1.length;i++)
      {
         for(int j=0;j<array2.length;j++)
